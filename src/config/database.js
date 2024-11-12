b2b9b9c1df3c6669e6b176ca340baa392af72655
Ayush Mangukiya <105487615+Ayush-Mangukiya@users.noreply.github.com>
2024-11-12 11:22:57 +0530

    Security fix

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://ayushmangukiya:kWxM9IlDXFDqinM4@cluster0.fscts.mongodb.net/devTinder"
-  );
-};
-
-module.exports = connectDB;
